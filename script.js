6a29d897c8a3b52e1701523d1de1df0a0584524b
Vlad27aa <95078355+Vlad27aa@users.noreply.github.com>
2022-06-29 02:37:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,25 +3,28 @@ document.querySelector('.button').onclick = check
 function check() {
     const income = Number(document.querySelector('.income').value)
     const tips = Number(document.querySelector('.tips').value)
-    let distance = Number(document.querySelector('.distance').value)
+    const distance = Number(document.querySelector('.distance').value)
     const fuelPrice = Number(document.querySelector('.fuelPrice').value)
     const fuelСonsumption = Number(document.querySelector('.fuelСonsumption').value)
     const hoursWorked = Number(document.querySelector('.hoursWorked').value)
-    const amortization = distance * 1.5
+    const dinner = Number(document.querySelector('.dinner').value)
+    const amortizationValue = Number(document.querySelector('.amortizationValue').value)
+    const amortization = distance * amortizationValue
     const costOneKm = fuelPrice * fuelСonsumption / 100
     const costFullDistance = Math.round(costOneKm * distance)
-    let profit = income + tips - costFullDistance - amortization
-    let coefficient = (profit / distance).toFixed(2)
+    const profit = income + tips - costFullDistance - amortization - dinner
 
     const outResult = 
     'Касса с чаевыми - ' + (income + tips) + ' грн' + '<br>' +
-    `Стоимость одного километра - ${costOneKm} грн` + '<br>' +
     `Затраты на топливо - ${costFullDistance} грн` + '<br>' + 
-    `Аммортизация (1.5 грн/км) - ${amortization} грн` + '<br>' +
-    `Коэфициент - ${coefficient}` + '<br>' +
+    `Амортизация (${amortizationValue} грн/км) - ${amortization} грн` + '<br>' +
     'Чистый заработок в час - ' + Math.round(profit / hoursWorked) + ' грн' + '<br>' +
     `Чистый заработок - ${profit} грн`
 
+    
+
     document.querySelector('.out').innerHTML = outResult
+   
+    
 
 }
